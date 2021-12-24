d6b65cad39c53db9fe2aced5ae4706a4495df044
sanjipun <iamsanjipun@gmail.com>
2021-12-24 19:31:48 +0545

    add tailwind congfig

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkGrey: '#141e30 ',
+        lightGrey: '#243b55'
+      }
+    },
+  },
+  plugins: [],
+}
