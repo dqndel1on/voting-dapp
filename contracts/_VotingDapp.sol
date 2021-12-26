7ef9d763ff97c3a7bb538fe8fc03e037879f7e5a
sanjipun <iamsanjipun@gmail.com>
2021-12-26 19:12:14 +0545

    update smart contract

---
+++ contracts/_VotingDapp.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract VotingDapp {
+    uint256 totalVotes=0;
+    uint256 totalCandidates=0;
+    bool electionStarted=false;
+    address manager;
+
+    struct Candidate {
+        string candidateName;
+        uint256 candidateAge;
+        uint256 totalVotes;
+    }
+
+    Candidate[] public Candidates;
+
+    modifier isManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    modifier isElectionActive(bool _value) {
+        require(electionStarted == _value);
+        _;
+    }
+
+    function startElection() external {
+        // start election. The election should not be active.
+        electionStarted = true;
+        manager = msg.sender;
+    }
+
+    function endElection() external isManager{
+        // end election. The election should be active. Only manager can end election.
+    }
+
+    function becomeCandidate() external {
+        // register as candidate. The election should not be active.
+    }
+
+    function vote() external {
+        // vote candidate. The election should be active.
+    }
+
+    function currentStatus() external {
+        // get total number of votes of each candidate at the moment. The election should be active. Anyone can see current status.
+    }
+
+    function getResult() external {
+        // get winner of the election. The election should not be active. Anyone can see the result.
+    }
+}
